f78e3923a30020b4e4f96b893ead9fa297967e04
Amaan <am235497@dal.ca>
2025-09-03 16:40:35 -0300

    learnt about nodemailer and implemented it

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const app = express();
 const port = 3000;
+const JWT_SECRET = 'i@will*getco_op_hopefully';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'groupscheduler846@gmail.com', pass: 'ubkcahiwygrzblvb' 
+  }
+});
 
 // Middleware to parse JSON requests
 app.use(express.json());
